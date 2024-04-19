3bc2d65fe0c320cace69530b5bc4666b3ab64c52
Buzhak <Byzhak@ya.ru>
2024-04-19 12:44:17 +0300

    start bot

---
+++ bot/main.py
@@ -0,0 +1,27 @@
+import asyncio
+import logging
+import os
+
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+
+
+import hendlers as hendlers
+
+load_dotenv()
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+
+    bot = Bot(token=os.getenv('TOKEN'), parse_mode='HTML')
+    dp = Dispatcher()
+    dp.include_routers(
+        hendlers.router
+    )
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
